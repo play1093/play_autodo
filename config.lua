8fa60ce8ceeb1901eb6183a55924bceef4b567a3
play1093 <83318035+play1093@users.noreply.github.com>
2021-07-23 17:00:25 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Time = 40000
+
+Config.Command = "autodo"
+
+Config.CommandText = "autodo_text"
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'play1093#8607'
+
+description 'take a better rp'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+
